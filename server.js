0db529dfcd24a12b82350b36a0bb097922fb704d
will1351 <38027330+will1351@users.noreply.github.com>
2019-06-18 16:21:37 +0930

    reverting to original

--- server.js
+++ server.js
@@ -1,62 +1,16 @@
-// @ts-check
-var util = require('./util');
-const express = require('express');
-const http = require('http');
-const url = require('url'); 
-var cookieParser = require('cookie-parser');
-const request = require('request');
-const cors = require('cors');
-const db = require('./queries');
+const express = require('express')
+const bodyParser = require('body-parser')  // for uploads (POST)
+const cors = require('cors')
+const db = require('./queries')
 
+const app = express()
 
-
-async function main() {
-  // Azure App Service will set process.env.port for you, but we use 3000 in development.
-  const PORT = process.env.PORT || 3000;
-  // Create the express routes
-  let app = express();
-
-  var corsOptions = {
+var corsOptions = {
     origin: 'http://se4a.azurewebsites.net',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 
 app.use(cors(corsOptions))
-
-
-  app.use(express.static('public'));
-  app.use(cookieParser());
-
-  app.get('/', async (req, res) => {
-    if(req.query && req.query.loginsession)
-    {
-    res.cookie('loginsession',req.query.loginsession, { maxAge: 3600000, httpOnly: true, })
-    res.redirect(url.parse(req.url).pathname);
-    }
-    else
-    {
-    let indexContent = await util.loadEnvironmentVariables({host: process.env['HTTP_HOST']});
-    res.end(indexContent);
-    }
-  });
-
-  app.get('/api/metadata', async (req, res) => {
-    if (req.cookies.loginsession) {
-      let tryappserviceendpoint= (process.env['APPSETTING_TRYAPPSERVICE_URL'] || 'https://tryappservice.azure.com') + '/api/vscoderesource';
-      const options = {
-        url: tryappserviceendpoint,
-        headers:{
-            cookie: 'loginsession='+req.cookies.loginsession
-        }
-      };
-      
-  const x =request(options);
-  x.pipe(res);
-}
-else{
-  res.end(404);
-}
-});
 
 
 
@@ -67,11 +21,7 @@ app.get('/api/committee/address/:address_id', db.getCommitteeAddress)
 app.get('/api/committee/members/:committee_id', db.getCommitteeMembers)
 app.get('/api/committee/documents/:committee_id', db.getCommitteeDocuments)
 
-// Create the HTTP server.
-  let server = http.createServer(app);
-  server.listen(PORT, function () {
-    console.log(`Listening on port ${PORT}`);
-  });
-}
+app.listen(8000, () => {
+    console.log('Server started!')
+})
 
-main();
